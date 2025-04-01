1bb3eb4edc94e7d43da7b8ed2d8d22744f948925
bon2dev <jonellebong07@gmail.com>
2025-04-02 00:14:32 +0800

    changes to main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(input_path):
+    with open(input_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+main()
+
